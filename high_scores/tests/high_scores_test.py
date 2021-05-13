1301b52c9747a62413edbd2da503d9a795c6e1ea
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-13 12:19:53 +0100

    first test working

--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -8,10 +8,14 @@ from src.high_scores import latest, personal_best, personal_top_three
 class HighScoresTest(unittest.TestCase):
     
     # Tests
+    def setUp(self):
+        self.scores = [5,2,8,21,10,6]
 
     # Test latest score (the last thing in the list)
-
+    def test_latest_score(self):
+        self.assertEqual(6, latest(self.scores)) #this is me checking the result is 6, from the scores list, when i use the function in high_scores.py
     # Test personal best (the highest score in the list)
+
 
     # Test top three from list of scores
 
@@ -22,4 +26,3 @@ class HighScoresTest(unittest.TestCase):
     # Test top three when there are less than three
 
     # Test top three when there is only one
-    
